0e7f5867522ab46c9be3f0aea036ef569cedee2c
Ankit_Pa <ankit64291@gmail.com>
2026-04-29 22:12:36 +0530

    Added CI workflow

--- DriverFactory/BroswerFactory.py
+++ DriverFactory/BroswerFactory.py
@@ -6,6 +6,8 @@ class DriverFactory:
         options = webdriver.ChromeOptions()
         options.add_argument("--maximize")
         options.add_argument("--incognito")
-        options.add_argument("--headless")
+        options.add_argument("--headless = new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
         driver = webdriver.Chrome(options = options)
         return driver
