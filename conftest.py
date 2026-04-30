7da57afadffd1143fccd72348f18dc8e6326ba2d
Ankit_Pa <ankit64291@gmail.com>
2026-04-30 10:25:10 +0530

    added hookwrapper

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from DriverFactory import BroswerFactory as bf
-
+import allure
+from selenium.webdriver.remote.webdriver import WebDriver
 
 @pytest.fixture
 def browser_initialization(request):
@@ -9,3 +10,19 @@ def browser_initialization(request):
     yield driver
     driver.quit()
 
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+
+    # Only act after test call (not setup/teardown)
+    if report.when == "call" and report.failed:
+        driver = item.funcargs.get("driver", None)
+
+        if isinstance(driver, WebDriver):
+            # Attach screenshot to Allure
+            allure.attach(
+                driver.get_screenshot_as_png(),
+                name="failure-screenshot",
+                attachment_type=allure.attachment_type.PNG
+            )
